51798fbcc1623a2bea219519e7a21f76c2031315
yangalexyangg <alex.yang1115@gmail.com>
2021-12-17 17:29:19 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fa3a7d49-9030-484e-8dec-a671f3346d86';
+export const MODAL_CLOSE_SEC = 2.5;
